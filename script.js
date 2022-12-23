afc0d8d189112d691f0020b952db47cff84f8849
Rachel Johnson <rachelktyjohnson@gmail.com>
2022-12-23 15:52:36 +1100

    functionality to swap between timeframes

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ fetch('data.json')
         console.log(data);
         data.map((category) => {
             let slug = category.title.toLowerCase().replaceAll(" ", "-")
+            let img_url = `images/icon-${slug}.svg`;
             let time_card = `
             <section 
                 class="time-card ${slug}"
                 style="
                     background:${category.color};
-                    background-image: url('images/icon-${slug}.svg');
+                    background-image: url(${img_url});
                     background-position: top -10px right 15px;
                     background-repeat: no-repeat;
                 "
@@ -34,3 +35,44 @@ fetch('data.json')
     })
 
 
+const time_period = document.querySelector(".time-period");
+
+
+function changeTimePeriod(el){
+    //remove active class from everyone
+    [...time_period.children].map((child)=>{
+        child.classList.remove("active");
+    })
+
+    //add active class to clicked item
+    el.target.classList.add("active");
+
+    fetch('data.json')
+        .then(data => data.json())
+        .then(data => {
+            data.forEach((category)=>{
+                let slug = category.title.toLowerCase().replaceAll(" ", "-")
+                let timeframe_data = category.timeframes[el.target.id]
+                document.querySelector(`.${slug} .content .data .current-hours`).innerText = `${timeframe_data.current}hrs`
+                let last_timeframe;
+                if (el.target.id === "daily"){
+                    last_timeframe = "Yesterday"
+                } else if (el.target.id === "weekly"){
+                    last_timeframe = "Last week";
+                } else {
+                    last_timeframe = "Last month"
+                }
+                document.querySelector(`.${slug} .content .data .previous-hours`).innerText = `${last_timeframe} - ${timeframe_data.previous}hrs`
+            })
+        })
+    //match to fetched data with index
+    //find the timeframe that matches el.target.id
+    //change contents of card's current hours and previous hours
+}
+
+//assign event listeners to each time period button
+[...time_period.children].map((child)=>{
+    child.addEventListener('click', (el) => {
+        changeTimePeriod(el)
+    })
+})
